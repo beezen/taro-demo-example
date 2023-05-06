44c4048a457323cb0ace41fa15e544c6916bfd8c
beezen <1184031131@qq.com>
2023-05-06 09:19:18 +0800

    feat: 新增3.6.6版本onPageScroll事件不生效demo-vue2-h5

---
+++ taro-3.6.6-pageScroll/src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+    <view class="block">block</view>
+    <view class="block">block</view>
+    <view class="block">block</view>
+    <view class="block">block</view>
+    <view class="block">block</view>
+    <view class="block">block</view>
+    <view class="block">block</view>
+    <view class="block">block</view>
+    <view class="block">block</view>
+    <view class="block">block</view>
+    <view class="block">block</view>
+  </view>
+</template>
+
+<script>
+import "./index.less";
+export default {
+  data() {
+    return {
+      msg: "pageScroll",
+    };
+  },
+  onPageScroll(e) {
+    console.warn("onPageScroll", e);
+  },
+  onPullDownRefresh() {
+    console.warn("onPullDownRefresh");
+  },
+};
+</script>
