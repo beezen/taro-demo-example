1d725aa9246b057efbc8d903e0902fa8ad41e03c
beezen <1184031131@qq.com>
2023-06-27 15:00:55 +0800

    chore: v3.5.6 版本工程名称不同,H5打包产物不同

---
+++ 1taro123/src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './index.less'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
